cad523d35bbab1b677ce0005d427df94a438a53a
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-06 23:48:15 +0100

    font-family nunito added

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         margin: 0;
         padding: 0;
-        font-family: "Montserrat", sans-serif;
+        font-family: "Nunito", sans-serif;
     }
 
     ::after, ::before {
