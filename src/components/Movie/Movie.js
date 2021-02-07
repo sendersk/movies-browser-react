5afa3acd489b6f8fa071b6ec3ce6f937138bc94d
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-07 18:09:55 +0100

    img source added

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function Movie() {
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+function Movie({title, poster_path, overview, vote_average}) {
     return (
         <div>
-            This is a movie component            
+            <img src={IMG_API + poster_path} alt={title} />            
         </div>
     )
 }
