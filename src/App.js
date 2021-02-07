b4cff794773cacde7bb18c3555bc1052f2151068
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-07 16:59:16 +0100

    Movie component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react"
+import Movie from "./components/Movies/Movie"
 
 function App() {
+  const movies = ['1', '2', '3'];
+
   return (
     <div>
+      {movies.map(movie => (<Movie />))}
     </div>
   );
 }
