c6d9da21ed4d0e71ce9723bad71c5a236e5d35ba
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-07 17:49:44 +0100

    FEATURED_API, IMG_API, SEARCH_API added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react"
-import Movie from "./components/Movies/Movie"
+import React from "react";
+import Movie from "./components/Movie/Movie";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f271630baa852ad3c9f1b5da3a4d53aa&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=f271630baa852ad3c9f1b5da3a4d53aa&query=";
 
 function App() {
-  const movies = ['1', '2', '3'];
+  const movies = ["1", "2", "3"];
 
   return (
     <div>
-      {movies.map(movie => (<Movie />))}
+      {movies.map((movie) => (
+        <Movie />
+      ))}
     </div>
   );
 }
