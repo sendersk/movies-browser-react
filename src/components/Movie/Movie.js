84b1f5aefd1a51d65784f36b5cef1007a8dd4154
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-07 18:40:55 +0100

    Movie content added

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Wrapper, Image, Info, Header } from "./styled";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-function Movie({title, poster_path, overview, vote_average}) {
-    return (
-        <div>
-            <img src={IMG_API + poster_path} alt={title} />            
-        </div>
-    )
+function Movie({ title, poster_path, overview, vote_average }) {
+  return (
+    <Wrapper>
+        <Image src={IMG_API + poster_path} alt={title} />
+        <Info>
+          <Header>{title}</Header>
+          <span>{vote_average}</span>
+        </Info>
+    </Wrapper>
+  );
 }
 
-export default Movie
+export default Movie;
