f592cc2b605dde780aa58ff371b1adcacca68714
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-08 22:24:53 +0100

    transform on hover added

--- src/components/Movie/styled.js
+++ src/components/Movie/styled.js
@@ -1,13 +1,31 @@
 import styled from "styled-components";
 
+export const Overview = styled.div`
+  background-color: #fff;
+  color: #22254b;
+  padding: 1rem;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  transform: translateX(-100%);
+`;
+
 export const Wrapper = styled.div`
   background-color: #373b69;
   border-radius: 3px;
-  box-shadow: 3px 3px 5px rgba(0,0,0,0.1);
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.1);
   overflow: hidden;
   position: relative;
   margin: 1rem;
   width: 22%;
+
+  &:hover {
+    ${Overview} {
+      transform: translateX(0%);
+      transition: .3s ease-out;
+    }
+  }
 `;
 
 export const Image = styled.img`
@@ -24,17 +42,3 @@ export const Info = styled.div`
 export const Header = styled.h3`
   margin: 0;
 `;
-
-export const Overview = styled.div`
-  background-color: #fff;
-  color: #22254b;
-  padding: 1rem;
-  position: absolute;
-  bottom:0;
-  left: 0;
-  right:0;
-
-  
-  transform: translateX(100%);
-  
-`
