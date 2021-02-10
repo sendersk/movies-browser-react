85fd8bcfba70e4960706c2029e901b38dd361010
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-10 18:05:29 +0100

    Navigation component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie/Movie";
 import { Container } from "./components/Container/styled";
-import Search from "./components/Search/Search";
+import Navigation from "./components/Navigation/Navigation";
 
 const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f271630baa852ad3c9f1b5da3a4d53aa&page=1";
@@ -23,7 +23,7 @@ function App() {
   return (
     <>
       <Container>
-        <Search />
+        <Navigation />
         {movies.length > 0 &&
           movies.map((movie) => <Movie key={movie.id} {...movie} />)}
       </Container>
