e7e1d5c8b34a22227b2b73255c278554d642ea0d
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-10 18:05:08 +0100

    Navigation component created

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from "../Search/Search"
+import {Wrapper} from "./styled"
+
+function Navigation() {
+    return (
+        <Wrapper>
+            <Search />
+        </Wrapper>
+    )
+}
+
+export default Navigation
---
+++ src/components/Navigation/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 60px;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: #373b69;
+`;
