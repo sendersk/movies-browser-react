bfe2443a4afaa58b97d47093241b2759ba315347
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-10 18:15:18 +0100

    Search styles-component created

---
+++ src/components/Search/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+    margin-left: 40px;
+    padding: 20px;
+
+`
+
+export const Input = styled.input`
+    font-size: 20px;
+    padding: 3px;
+`
