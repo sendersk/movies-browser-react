9ad71ed8c14ae2b302e81f6d751202cfb0ffa13d
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-10 18:14:23 +0100

    Search component styles added

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Wrapper, Input} from "./styled"
 
 function Search() {
     return (
-        <header>
-            <input type="search" placeholder="Search..." />
-        </header>
+        <Wrapper>
+            <Input type="search" placeholder="Search..." />
+        </Wrapper>
     )
 }
 
