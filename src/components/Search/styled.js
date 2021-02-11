fa45172e0f12f971c528440743518992e641a2b2
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-11 19:34:50 +0100

    input styles modified

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -1,11 +1,14 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.div`
-    padding: 20px;
-
+    margin-right: 30px;
 `
 
 export const Input = styled.input`
+    background-color: transparent;
+    color: #22254b;
     font-size: 20px;
-    padding: 3px;
+    padding: 6px 12px;
+    border: 2px solid #22254b; 
+    border-radius: 50px;
 `
