12b54bf73a52a358b520ca7e52a6605728e24247
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-11 19:29:18 +0100

    searchbar position changed

--- src/components/Navigation/styled.js
+++ src/components/Navigation/styled.js
@@ -5,5 +5,6 @@ export const Wrapper = styled.div`
   width: 100%;
   height: 60px;
   align-items: center;
+  justify-content: flex-end;
   background-color: #373b69;
 `;
--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.div`
-    margin-left: 40px;
     padding: 20px;
 
 `
