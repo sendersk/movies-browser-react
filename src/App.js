57e5b61c293dd99cc32ee6db12c9f59f201a2735
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-12 18:42:00 +0100

    SEARCH_API moved to Search component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Navigation from "./components/Navigation/Navigation";
 
 const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f271630baa852ad3c9f1b5da3a4d53aa&page=1";
-const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?&api_key=f271630baa852ad3c9f1b5da3a4d53aa&query=";
-
+  
 function App() {
   const [movies, setMovies] = useState([]);
 
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Wrapper, Input } from "./styled";
 
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=f271630baa852ad3c9f1b5da3a4d53aa&query=";
+
 function Search() {
   const [searchTerm, setSearchTerm] = useState("");
 
