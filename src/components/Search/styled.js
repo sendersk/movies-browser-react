c13911433bafe183875998b89026bad9a2898b1a
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-14 19:08:31 +0100

    NavigationContainer added

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -1,14 +1,23 @@
-import styled from "styled-components"
+import styled from "styled-components";
+
+export const NavigationContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 60px;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: #373b69;
+`;
 
 export const Wrapper = styled.div`
-    margin-right: 30px;
-`
+  margin-right: 30px;
+`;
 
 export const Input = styled.input`
-    background-color: transparent;
-    color: #22254b;
-    font-size: 20px;
-    padding: 6px 12px;
-    border: 2px solid #22254b; 
-    border-radius: 50px;
-`
+  background-color: transparent;
+  color: #22254b;
+  font-size: 20px;
+  padding: 6px 12px;
+  border: 2px solid #22254b;
+  border-radius: 50px;
+`;
