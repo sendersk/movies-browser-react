e238953411d794a6bf38036ff2fc30464bc921d8
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-14 19:18:58 +0100

    getMovies function added

--- src/App.js
+++ src/App.js
@@ -18,27 +18,26 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetch(FEATURED_API)
+    getMovies(FEATURED_API)
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setMovies(data.results);
       });
-  }, []);
+  }
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
-    if(searchTerm){
-      fetch(SEARCH_API+searchTerm)
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-      });
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm)
 
-      setSearchTerm("");
+        setSearchTerm("");
     }
-    
   };
 
   const handleOnChange = (e) => {
